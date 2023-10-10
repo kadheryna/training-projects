a563bc443f018dbf30cb674705ea89f7d5e8e812
kadheryna <katost95@gmail.com>
2023-10-10 18:38:58 +0200

    Implement page design

---
+++ Project-5/tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			backgroundImage: {
+				work: "url('./images/icon-work.svg')",
+				play: "url('./images/icon-play.svg')",
+				selfCare: "url('./images/icon-self-care.svg')",
+				social: "url('./images/icon-social.svg')",
+				study: "url('./images/icon-study.svg')",
+				exercise: "url('./images/icon-exercise.svg')",
+			},
+			colors: {
+				bgBody: "hsl(226, 43%, 10%)",
+				bgPerson: "hsl(246, 80%, 60%)",
+				darkBlue: "hsl(235, 46%, 20%)",
+				paleBlue: "hsl(236, 100%, 87%)",
+				desaturatedBlue: "hsl(235, 45%, 61%)",
+				lightRedWork: "hsl(15, 100%, 70%)",
+				softBlue: "hsl(195, 74%, 62%)",
+				lightRedStudy: "hsl(348, 100%, 68%)",
+				limeGreen: "hsl(145, 58%, 55%)",
+				violet: "hsl(264, 64%, 52%)",
+				softOrange: "hsl(43, 84%, 65%)",
+			},
+			fontSize: {
+				light: 300,
+				medium: 400,
+				semibold: 500,
+			},
+		},
+	},
+	plugins: [],
+};
